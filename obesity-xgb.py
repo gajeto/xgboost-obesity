1941ea86124d9f9393a6921166264921459f91a8
Gustavo Jerez <57077175+gajeto@users.noreply.github.com>
2021-05-01 10:52:13 -0500

    Working encoding for dataset

--- obesity-xgb.py
+++ obesity-xgb.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from pandas import read_csv
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
@@ -10,22 +10,23 @@ from sklearn.preprocessing import OneHotEncoder
 data = read_csv('C:\\Users\GUSTAVOJEREZ\PycharmProjects\XGBOOST\data_obesity.csv', header=None)
 dataset = data.values
 
-X = dataset[:, :-1].astype(str)
+X1 = dataset[:, :-2].astype(str)
+X2 = dataset[:,-2].astype(str)
 Y = dataset[:, -1]
 
 ordinal_encoder = OrdinalEncoder()
-X = ordinal_encoder.fit_transform(X[:,8:-1])
+X1 = ordinal_encoder.fit_transform(X1[:,8:])
 
 onehot_encoder = OneHotEncoder(sparse=False)
-X[:,-1] = X[:,-1].reshape(X.shape[0], 1)
-transp = onehot_encoder.fit_transform(X[:,-1])
-X = numpy.concatenate((X, transp), axis=1)
-# ordinal encode target variable
-label_encoder = LabelEncoder()
-y = label_encoder.fit_transform(Y)
-# summarize the transformed data
+X2 = X2.reshape(X1.shape[0],1)
+X2 = onehot_encoder.fit_transform(X2)
 
-print('Input', X.shape)
-print(X[:5, :])
-print('Output', y.shape)
-print(y[:5])
+label_encoder = LabelEncoder()
+Y= label_encoder.fit_transform(Y)
+
+print('Input X1', X1.shape)
+print(X1[:5, :])
+print('Input X2', X2.shape)
+print(X2[:5, :])
+print('Output', Y.shape)
+print(Y[:5])
